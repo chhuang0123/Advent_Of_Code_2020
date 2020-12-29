966e2b309ebc45bb3a427fe5e5d45332a0367c70
Eric C Huang <eric_c_huang@trendmicro.com>
2020-12-29 22:26:38 +0800

    complete day1 part2

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn find_sum(target_sum: i32, numbers: HashMap<i32, bool>) -> (i32, i32) {
+fn find_sum(target_sum: i32, numbers: &HashMap<i32, bool>) -> (i32, i32) {
     for (key, _) in numbers.iter() {
         let target_number = target_sum - key;
         if numbers.contains_key(&target_number) {
@@ -16,15 +16,52 @@ fn find_sum(target_sum: i32, numbers: HashMap<i32, bool>) -> (i32, i32) {
 }
 
 fn main() {
-    let filename = String::from("/tmp/input");
-    let file = File::open(filename).unwrap();
+    // 2 entries
+    let filename_2 = String::from("/tmp/input.2");
+    let file = File::open(filename_2).unwrap();
     let reader = BufReader::new(file);
-    let mut numbers: HashMap<i32, bool> = HashMap::new();
+    let mut numbers_2: HashMap<i32, bool> = HashMap::new();
 
     for (index, line) in reader.lines().enumerate() {
-        numbers.insert(line.unwrap().parse::<i32>().unwrap(), true);
+        numbers_2.insert(line.unwrap().parse::<i32>().unwrap(), true);
     }
 
-    let result = find_sum(2020, numbers);
-    println!("{} * {} = {}", result.0, result.1, result.0 * result.1);
+    let result_2 = find_sum(2020, &numbers_2);
+    if result_2 != (0, 0) {
+        println!(
+            "{} * {} = {}",
+            result_2.0,
+            result_2.1,
+            result_2.0 * result_2.1
+        );
+    }
+
+    // 3 entries
+    let filename_3 = String::from("/tmp/input.3");
+    let file = File::open(filename_3).unwrap();
+    let reader = BufReader::new(file);
+    let mut numbers_3: HashMap<i32, bool> = HashMap::new();
+
+    for (index, line) in reader.lines().enumerate() {
+        numbers_3.insert(line.unwrap().parse::<i32>().unwrap(), true);
+    }
+
+    let mut result_3 = (0, 0, 0);
+    for (number, _) in numbers_3.iter() {
+        let max_sum: i32 = 2020 - number;
+        let tmp_result = find_sum(max_sum, &numbers_3);
+        if tmp_result != (0, 0) {
+            result_3 = (number.clone(), tmp_result.0, tmp_result.1);
+        }
+    }
+
+    if result_3 != (0, 0, 0) {
+        println!(
+            "{} * {} * {} = {}",
+            result_3.0,
+            result_3.1,
+            result_3.2,
+            result_3.0 * result_3.1 * result_3.2
+        );
+    }
 }
