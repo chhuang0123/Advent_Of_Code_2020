6735804d7b74b64c5f158cb180a76fa5158c0f25
Eric C Huang <eric_c_huang@trendmicro.com>
2020-12-31 14:18:52 +0800

    finish day 05

---
+++ day_05/src/lib.rs
@@ -0,0 +1,40 @@
+#[test]
+fn test_parse_row_success() {
+    assert_eq!(parse_row(&"FBFBBFF"), 44);
+}
+
+#[allow(dead_code)]
+fn parse_row(_source: &str) -> i32 {
+    let mut source: String = String::from(_source);
+    source = source.replace('B', "1");
+    source = source.replace('F', "0");
+
+    i32::from_str_radix(&source, 2).unwrap()
+}
+
+#[test]
+fn test_parse_column_success() {
+    assert_eq!(parse_column(&"RLR"), 5);
+}
+
+#[allow(dead_code)]
+fn parse_column(_source: &str) -> i32 {
+    let mut source: String = String::from(_source);
+    source = source.replace('R', "1");
+    source = source.replace('L', "0");
+
+    i32::from_str_radix(&source, 2).unwrap()
+}
+
+#[test]
+fn test_parse_boarding_string_success() {
+    assert_eq!(parse_boarding_string(&"FBFBBFFRLR"), (44, 5));
+}
+
+#[allow(dead_code)]
+pub fn parse_boarding_string(source: String) -> (i32, i32) {
+    let _row_string = &source.get(0..7).unwrap();
+    let _column_string = &source.get(7..10).unwrap();
+
+    (parse_row(_row_string), parse_column(_column_string))
+}
---
+++ day_05/src/main.rs
@@ -0,0 +1,56 @@
+#![allow(dead_code, unused_imports, unused_mut, unused_variables)]
+
+use day_05::parse_boarding_string;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+
+    // part 1
+    let filename = &args[0];
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut max_value = 0;
+    for (_, line) in reader.lines().enumerate() {
+        let row_column = parse_boarding_string(line.unwrap());
+        let result = row_column.0 * 8 + row_column.1;
+        if result > max_value {
+            max_value = result;
+        }
+    }
+    println!("part 1: {} is max value", max_value);
+
+    // part 2
+    let filename = &args[1];
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut min_value: i32 = 10000000;
+    let mut max_value: i32 = 0;
+    let mut seats: Vec<i32> = Vec::new();
+    for (_, line) in reader.lines().enumerate() {
+        let row_column: (i32, i32) = parse_boarding_string(line.unwrap());
+        let result: i32 = row_column.0 * 8 + row_column.1;
+
+        seats.push(result);
+
+        if result > max_value {
+            max_value = result;
+        }
+
+        if result < min_value {
+            min_value = result;
+        }
+    }
+
+    for i in min_value..max_value {
+        if seats.contains(&i) {
+            continue;
+        }
+
+        println!("part 2: {} is my seat id", i);
+        break;
+    }
+}
