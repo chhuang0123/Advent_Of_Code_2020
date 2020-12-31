a2e816076d3e32f9cb8351ec4f7b86bd5125548c
Eric C Huang <eric_c_huang@trendmicro.com>
2020-12-31 12:27:45 +0800

    add test script

--- day_04/src/lib.rs
+++ day_04/src/lib.rs
@@ -5,6 +5,24 @@ fn is_valid_filed_name(value: &str) -> bool {
     valid_values.contains(&value)
 }
 
+#[test]
+fn test_is_valid_filed_name_success() {
+    assert_eq!(is_valid_filed_name("byr"), true);
+    assert_eq!(is_valid_filed_name("iyr"), true);
+    assert_eq!(is_valid_filed_name("eyr"), true);
+    assert_eq!(is_valid_filed_name("hgt"), true);
+    assert_eq!(is_valid_filed_name("hcl"), true);
+    assert_eq!(is_valid_filed_name("ecl"), true);
+    assert_eq!(is_valid_filed_name("pid"), true);
+    assert_eq!(is_valid_filed_name("cid"), true);
+}
+
+#[test]
+fn test_is_valid_filed_name_failure() {
+    assert_eq!(is_valid_filed_name("abc"), false);
+    assert_eq!(is_valid_filed_name(""), false);
+}
+
 pub fn is_valid(source: &str) -> i32 {
     let re = Regex::new(r"(\S+):(\S+)").unwrap();
     let mut count = 0;
@@ -36,14 +54,74 @@ fn is_valid_byr(value: &str) -> bool {
     year >= 1920 && year <= 2002
 }
 
+#[test]
+fn test_is_valid_byr_success() {
+    assert_eq!(is_valid_byr("1920"), true);
+    assert_eq!(is_valid_byr("1921"), true);
+    assert_eq!(is_valid_byr("2001"), true);
+    assert_eq!(is_valid_byr("2002"), true);
+}
+
+#[test]
+fn test_is_valid_byr_failure() {
+    assert_eq!(is_valid_byr("1919"), false);
+    assert_eq!(is_valid_byr("2003"), false);
+    assert_eq!(is_valid_byr("-1"), false);
+    assert_eq!(is_valid_byr("0"), false);
+    assert_eq!(is_valid_byr("10"), false);
+    assert_eq!(is_valid_byr("100"), false);
+    assert_eq!(is_valid_byr("1000"), false);
+    assert_eq!(is_valid_byr("10000"), false);
+}
+
 fn is_valid_iyr(value: &str) -> bool {
     let year: i32 = value.parse().unwrap();
     year >= 2010 && year <= 2020
 }
 
+#[test]
+fn test_is_valid_iyr_success() {
+    assert_eq!(is_valid_iyr("2010"), true);
+    assert_eq!(is_valid_iyr("2011"), true);
+    assert_eq!(is_valid_iyr("2019"), true);
+    assert_eq!(is_valid_iyr("2020"), true);
+}
+
+#[test]
+fn test_is_valid_iyr_failure() {
+    assert_eq!(is_valid_iyr("2009"), false);
+    assert_eq!(is_valid_iyr("2021"), false);
+    assert_eq!(is_valid_iyr("-1"), false);
+    assert_eq!(is_valid_iyr("0"), false);
+    assert_eq!(is_valid_iyr("10"), false);
+    assert_eq!(is_valid_iyr("100"), false);
+    assert_eq!(is_valid_iyr("1000"), false);
+    assert_eq!(is_valid_iyr("10000"), false);
+}
+
 fn is_valid_eyr(value: &str) -> bool {
     let year: i32 = value.parse().unwrap();
     year >= 2020 && year <= 2030
+}
+
+#[test]
+fn test_is_valid_eyr_success() {
+    assert_eq!(is_valid_eyr("2020"), true);
+    assert_eq!(is_valid_eyr("2021"), true);
+    assert_eq!(is_valid_eyr("2029"), true);
+    assert_eq!(is_valid_eyr("2030"), true);
+}
+
+#[test]
+fn test_is_valid_eyr_failure() {
+    assert_eq!(is_valid_eyr("2019"), false);
+    assert_eq!(is_valid_eyr("2031"), false);
+    assert_eq!(is_valid_eyr("-1"), false);
+    assert_eq!(is_valid_eyr("0"), false);
+    assert_eq!(is_valid_eyr("10"), false);
+    assert_eq!(is_valid_eyr("100"), false);
+    assert_eq!(is_valid_eyr("1000"), false);
+    assert_eq!(is_valid_eyr("10000"), false);
 }
 
 fn is_valid_hgt(value_string: &str) -> bool {
@@ -71,9 +149,42 @@ fn is_valid_hgt(value_string: &str) -> bool {
     result
 }
 
+#[test]
+fn test_is_valid_hgt_success() {
+    assert_eq!(is_valid_hgt("150cm"), true);
+    assert_eq!(is_valid_hgt("190cm"), true);
+    assert_eq!(is_valid_hgt("193cm"), true);
+
+    assert_eq!(is_valid_hgt("59in"), true);
+    assert_eq!(is_valid_hgt("70in"), true);
+    assert_eq!(is_valid_hgt("76in"), true);
+}
+
+#[test]
+fn test_is_valid_hgt_failure() {
+    assert_eq!(is_valid_hgt("149cm"), false);
+    assert_eq!(is_valid_hgt("194cm"), false);
+
+    assert_eq!(is_valid_hgt("58in"), false);
+    assert_eq!(is_valid_hgt("77in"), false);
+}
+
 fn is_valid_hcl(value_string: &str) -> bool {
     let re = Regex::new(r"^#[0-9a-f]{6}").unwrap();
     re.is_match(value_string)
+}
+
+#[test]
+fn test_is_valid_hcl_success() {
+    assert_eq!(is_valid_hcl("#123abc"), true);
+    assert_eq!(is_valid_hcl("#abc123"), true);
+}
+
+#[test]
+fn test_is_valid_hcl_failure() {
+    assert_eq!(is_valid_hcl("123abc"), false);
+    assert_eq!(is_valid_hcl("#123abz"), false);
+    assert_eq!(is_valid_hcl("#abz123"), false);
 }
 
 fn is_valid_ecl(value: &str) -> bool {
@@ -81,27 +192,42 @@ fn is_valid_ecl(value: &str) -> bool {
     valid_values.contains(&value)
 }
 
+#[test]
+fn test_is_valid_ecl_success() {
+    assert_eq!(is_valid_ecl("amb"), true);
+    assert_eq!(is_valid_ecl("blu"), true);
+    assert_eq!(is_valid_ecl("brn"), true);
+    assert_eq!(is_valid_ecl("gry"), true);
+    assert_eq!(is_valid_ecl("grn"), true);
+    assert_eq!(is_valid_ecl("hzl"), true);
+    assert_eq!(is_valid_ecl("oth"), true);
+}
+
+#[test]
+fn test_is_valid_ecl_failure() {
+    assert_eq!(is_valid_ecl("abc"), false);
+}
+
 fn is_valid_pid(value: &str) -> bool {
     let re = Regex::new(r"^[0-9]{9}$").unwrap();
     re.is_match(value)
 }
 
+#[test]
+fn test_is_valid_pid_success() {
+    assert_eq!(is_valid_pid("000000000"), true);
+    assert_eq!(is_valid_pid("000000001"), true);
+    assert_eq!(is_valid_pid("999999999"), true);
+}
+
+#[test]
+fn test_is_valid_pid_failure() {
+    assert_eq!(is_valid_pid("0123456789"), false);
+}
+
 pub fn is_valid_part_2(source: &str) -> i32 {
     let re = Regex::new(r"(\S+):(\S+)").unwrap();
-    let mut count = 0;
-    let mut contain_cid = false;
-
-    for token in re.captures_iter(&source) {
-        if !is_valid_filed_name(&token[1]) {
-            return 0;
-        }
-
-        if &token[1] == "cid" {
-            contain_cid = true;
-        }
-
-        count += 1;
-    }
+    let count = re.captures_iter(&source).count();
 
     // println!("tokens: {}: ({}) ", source, count);
 
@@ -111,9 +237,14 @@ pub fn is_valid_part_2(source: &str) -> i32 {
     }
 
     // check fields
+    let mut contain_cid = false;
     for token in re.captures_iter(&source) {
         if !is_valid_filed_name(&token[1]) {
             return 0;
+        }
+
+        if &token[1] == "cid" {
+            contain_cid = true;
         }
 
         if &token[1] == "byr" && !is_valid_byr(&token[2]) {
