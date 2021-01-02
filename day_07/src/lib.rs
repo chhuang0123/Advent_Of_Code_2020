4c10e001027492ef55f5d23300e8f328b84b9ca0
Eric C Huang <eric_c_huang@trendmicro.com>
2021-01-02 23:53:10 +0800

    day 7 part 1

---
+++ day_07/src/lib.rs
@@ -0,0 +1,95 @@
+use regex::Regex;
+use std::collections::HashMap;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_rule_success() {
+        assert_eq!(
+            parse_rule(String::from(
+                "light red bags contain 1 bright white bag, 2 muted yellow bags."
+            )),
+            (
+                String::from("light red"),
+                vec![
+                    (1, String::from("bright white")),
+                    (2, String::from("muted yellow")),
+                ],
+            )
+        );
+
+        assert_eq!(
+            parse_rule(String::from("bright white bags contain 1 shiny gold bag.")),
+            (
+                String::from("bright white"),
+                vec![(1, String::from("shiny gold"))],
+            )
+        );
+
+        assert_eq!(
+            parse_rule(String::from("faded blue bags contain no other bags.")),
+            (String::from("faded blue"), vec![(0, String::from(""))])
+        );
+    }
+}
+
+fn parse_sub_bag_rule(_source: String) -> Vec<(usize, String)> {
+    let mut result: Vec<(usize, String)> = Vec::new();
+
+    let re = Regex::new(r"(\d+)\s(\S+\s\S+)\sbag").unwrap();
+    for tokens in re.captures_iter(&_source) {
+        let sub_bag_count: usize = String::from(&tokens[1]).parse().unwrap();
+        let sub_bag_name: String = String::from(&tokens[2]);
+        result.push((sub_bag_count, sub_bag_name));
+    }
+
+    result
+}
+
+pub fn parse_rule(_source: String) -> (String, Vec<(usize, String)>) {
+    let rule: Vec<&str> = _source.split(" bags contain ").collect();
+    // println!("{:?}", rule);
+
+    let bag = rule[0];
+    let sub_bag_text = rule[1];
+
+    if sub_bag_text.contains("no other bag") {
+        (String::from(bag), vec![(0, String::from(""))])
+    } else {
+        (
+            String::from(bag),
+            parse_sub_bag_rule(String::from(sub_bag_text)),
+        )
+    }
+}
+
+pub fn match_rule(key: String, rules: HashMap<String, Vec<(usize, String)>>) -> usize {
+    let mut bag_stack: Vec<String> = vec![key];
+    println!("-> start: {:?}", bag_stack);
+
+    while bag_stack.len() != 0 {
+        // println!("current: {:?}", bag_stack);
+
+        let tmp_key: String = bag_stack.pop().unwrap();
+        let rules = rules.get(&tmp_key).unwrap();
+
+        // println!("matched rules: {:?}", rules);
+
+        for rule in rules {
+            if rule.1 == "shiny gold" {
+                return 1;
+            }
+
+            if rule.0 == 0 {
+                continue;
+            }
+
+            // println!("add : {:?}", rule.1);
+            bag_stack.push(String::from(rule.1.clone()));
+        }
+    }
+
+    0
+}
---
+++ day_07/src/main.rs
@@ -0,0 +1,31 @@
+use day_07::{match_rule, parse_rule};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+
+    // part 1
+    let filename = &args[0];
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut rules: HashMap<String, Vec<(usize, String)>> = HashMap::new();
+    for (_, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        // println!("{:?}", line);
+
+        let rule = parse_rule(line);
+        // println!("{:?}", rule);
+
+        rules.insert(rule.0, rule.1);
+    }
+
+    let bags = rules.keys();
+    let mut total_valid = 0;
+    for key in bags {
+        total_valid += match_rule(String::from(key), rules.clone());
+    }
+    println!("part 1: {} total valid", total_valid);
+}
