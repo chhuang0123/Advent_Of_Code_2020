106c7c39dff08fbf56f436fa5c778b1a8038b2f2
Eric C Huang <eric_c_huang@trendmicro.com>
2021-01-05 08:36:19 +0800

    finish day 9 part 2

--- day_09/src/lib.rs
+++ day_09/src/lib.rs
@@ -1,4 +1,4 @@
-pub fn is_valid(target_index: usize, number_count: usize, numbers: &[u64]) -> bool {
+pub fn is_valid(number_count: usize, target_index: usize, numbers: &[u64]) -> bool {
     let target_sum = numbers[target_index];
 
     for i in (target_index - number_count)..target_index {
@@ -14,4 +14,26 @@ pub fn is_valid(target_index: usize, number_count: usize, numbers: &[u64]) -> bo
     }
 
     false
+}
+
+pub fn get_long_sum(number_count: usize, invalid_index: usize, numbers: &[u64]) -> Vec<u64> {
+    let invalid_number = numbers[invalid_index];
+
+    for i in 0..(invalid_index - number_count + 1) {
+        let mut result_numbers: Vec<u64> = Vec::new();
+        let mut tmp_sum: u64 = 0;
+        for j in 0..number_count {
+            result_numbers.push(numbers[i + j]);
+            tmp_sum += numbers[i + j];
+        }
+
+        if tmp_sum == invalid_number {
+            return result_numbers;
+        }
+        if tmp_sum >= invalid_number {
+            break;
+        }
+    }
+
+    vec![]
 }
--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -1,4 +1,4 @@
-use day_09::is_valid;
+use day_09::{get_long_sum, is_valid};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -19,12 +19,46 @@ fn main() {
     // println!("numbers: {:?}", numbers);
 
     let target_preamble = 25;
+    let mut invalid_number: u64 = 0;
+    let mut invalid_index: usize = 0;
     for i in target_preamble..numbers.len() {
-        if !is_valid(i, target_preamble, &numbers) {
-            println!(
-                "part 1: {} ({}) is not valid {}-number preamble.",
-                numbers[i], i, target_preamble
-            );
+        if !is_valid(target_preamble, i, &numbers) {
+            invalid_number = numbers[i];
+            invalid_index = i;
+            break;
         }
     }
+
+    println!(
+        "part 1: {} ({}) is not a valid {}-number preamble.",
+        invalid_number, invalid_index, target_preamble
+    );
+
+    // part 2
+    println!("part 2: sum of {} are", invalid_number);
+    let mut smallest_number: u64 = invalid_number;
+    let mut largest_number: u64 = 0;
+    for i in 3..target_preamble {
+        let result: Vec<u64> = get_long_sum(i, invalid_index, &numbers);
+        if !result.is_empty() {
+            for item in result.iter().take(result.len()) {
+                println!("{}", item);
+
+                if item > &largest_number {
+                    largest_number = *item;
+                }
+
+                if item < &smallest_number {
+                    smallest_number = *item;
+                }
+            }
+        }
+    }
+    println!("----------");
+    println!(
+        "smallest number ({}) + largest number ({}) = ({})",
+        smallest_number,
+        largest_number,
+        smallest_number + largest_number
+    );
 }
